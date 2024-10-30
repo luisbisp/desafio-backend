6f6ebb8c7da1722e171ee6c314bdcb675a3125ca
Luis Bispo <luis.bispo176@gmail.com>
2024-10-30 16:05:40 -0300

    first commit

---
+++ app/Notifications/NotificationUser.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\User;
+
+
+class NotificationUser extends Notification
+{
+    use Queueable;
+
+    private $user;
+    public $email_model;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(User $user, $email_model)
+    {
+        $this->$user = $user;
+        $this->email_model = (array) $email_model;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject($this->email_model['subject'])
+                    ->line($this->email_model['title'])
+                    ->line($this->email_model['message']);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
