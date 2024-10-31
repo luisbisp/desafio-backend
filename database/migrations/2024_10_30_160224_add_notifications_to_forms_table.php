0155afde908f774d5c4c823daf97bc2c65b6ed53
Luis Bispo <luis.bispo176@gmail.com>
2024-10-31 15:29:23 -0300

    fix: :bug: set notifications nullable

--- database/migrations/2024_10_30_160224_add_notifications_to_forms_table.php
+++ database/migrations/2024_10_30_160224_add_notifications_to_forms_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('forms', function (Blueprint $table) {
-            $table->json('notification');
+            $table->json('notification')->nullable();
         });
     }
 
