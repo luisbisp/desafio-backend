2c97e425cceea3a37f75c09e5994d576929c3996
Luis Bispo <luis.bispo176@gmail.com>
2024-11-22 16:00:05 -0300

    Reapply "refactor: :test_tube: test create metric"
    This reverts commit 2c3067dc1526bce6862bc6f935ad81a679f66154.

--- tests/Feature/MetricsTest.php
+++ tests/Feature/MetricsTest.php
@@ -11,21 +11,34 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 use App\Services\MetricsService;
+use Illuminate\Support\Facades\Log;
 
 class MetricsTest extends TestCase
 {
 
     use RefreshDatabase;
 
-    public function test_create_total_time(){
+    /** @test */
+    public function test_create_total_time_and_increment_respondent(){
 
         $user = User::factory()->create();
-        $form = Form::factory()->create([
-            'created_at' => "2024-11-19T20:25:36.000000Z",
-            'completed_at' => "2024-11-19T20:28:36.000000Z",
+        $form = Form::factory()->for($user)->create([
+            'notification' => [
+				'email' => false,
+				'whatsapp' => false,
+				'respondent_email' => false,
+				'webhook' => [
+					'active' => false,
+					'url' => null,
+				]
+			]
         ]);
 
-        $respondent = Respondent::factory()->for($form)->create();
+
+        $respondent = Respondent::factory()->for($form)->create([
+            'created_at' => now()->subMinutes(3),
+            'completed_at' => now(),   
+        ]);
         $answer = Answer::factory()->for($form)->for($respondent)->make();
 		$answer->is_last = true;
 
@@ -33,13 +46,23 @@ class MetricsTest extends TestCase
         
         $this->assertNotNull($post['data']['respondent']);
 
-        $metricService = new MetricsService();
-        $newMetric = $metricService->updateFormTime($answer);
+        $this->assertDatabaseHas('form_metrics', [
+            'form_id' => $form->slug,
+            'total_time' => 180,
+            'total_respondents' => 1,
+        ]);
 
-        $this->assertNotNull($newMetric);
-        $this->assertEquals(1, $newMetric->respondents);
-        $this->assertEquals(180, $newMetric->timeToComplete);
+        //testando se o incremento funciona
+        $answer = Answer::factory()->for($form)->for($respondent)->make();
+        $answer->is_last = true;
 
+        $post = $this->post('/api/answers', $answer->toArray());
+
+        $this->assertDatabaseHas('form_metrics', [
+            'form_id' => $form->slug,
+            'total_time' => 360,
+            'total_respondents' => 2,
+        ]);
     }
 
 }
