f38430507680e7c5a4868f30298446190c259346
Luis Bispo <luis.bispo176@gmail.com>
2024-11-29 13:49:01 -0300

    test: :test_tube: add test check remove asnwer_metric if field removed

--- tests/Feature/AnswersMetricsTest.php
+++ tests/Feature/AnswersMetricsTest.php
@@ -59,10 +59,10 @@ class AnswersMetricsTest extends TestCase
         $post->assertStatus(200);
 
         Bus::assertDispatched(UpdateViewAnswerMetrics::class);
-
     }
 
-    public function test_update_view_answer_metrics(){
+    public function test_update_view_answer_metrics()
+    {
 
         $user = User::factory()->create();
         $form = Form::factory()->for($user)->create();
@@ -75,20 +75,19 @@ class AnswersMetricsTest extends TestCase
         ];
 
         $requestKey = 'answer_metrics_' . uniqid();
-		$expirationTime = 300;
+        $expirationTime = 300;
         Redis::setex($requestKey, $expirationTime, json_encode($answer));
         UpdateViewAnswerMetrics::dispatchSync($requestKey);
 
         $numViews = 1;
         $numSubmits = 0;
-    
+
         $this->assertDatabaseHas('answers_metrics', [
             'form_id' => $form->slug,
             'field_id' => $fieldId,
             'views' => $numViews,
             'submits' => $numSubmits
         ]);
-
     }
 
 
@@ -156,7 +155,8 @@ class AnswersMetricsTest extends TestCase
     }
 
     /** @test */
-    public function test_can_show_answers_metrics(){
+    public function test_can_show_answers_metrics()
+    {
 
         $user = User::factory()->create();
         $form = Form::factory()->for($user)->create();
@@ -167,17 +167,62 @@ class AnswersMetricsTest extends TestCase
                 'field_id' => $field['field_id'],
             ]);
         }
-        
+
         $response = $this->actingAs($user)->get("/api/forms/$form->slug/metrics");
         $response->assertStatus(200);
     }
 
-    public function test_cant_show_answers_metrics_for_another_user(){
+    public function test_cant_show_answers_metrics_for_another_user()
+    {
 
         $user = User::factory()->create();
         $form = Form::factory()->for($user)->create();
         $response = $this->get("/api/forms/$form->slug/metrics");
         $response->assertStatus(401);
+    }
+
+    public function test_field_removed_from_form_remove_metrics_for_that_field()
+    {
+
+        $user = User::factory()->create();
+        $form = Form::factory()->for($user)->create();
+
+        foreach ($form->fields as $field) {
+            AnswersMetrics::factory()->for($form)->create([
+                'form_id' => $form->slug,
+                'field_id' => $field['field_id'],
+            ]);
+        }
+
+        $answersMetrics = AnswersMetrics::where('form_id', $form->slug)->get();
+
+        $this->actingAs($user)->put('/api/forms/' . $form->slug, [
+            'title' => 'Test form',
+            'show_time_to_complete' => false,
+            'notification' => [
+                'email' => false,
+                'whatsapp' => false,
+                'respondent_email' => false,
+                'webhook' => [
+                    'active' => false,
+                    'url' => 'test'
+                ]
+            ],
+            'fields' => [
+                [
+                    'label' => 'Qual seu nome?',
+                    'type' => 'text',
+                    'required' => false,
+                    'field_id' => $answersMetrics[0]->field_id
+                ]
+            ]
+        ]);
+
+
+        $this->assertDatabaseMissing('answers_metrics', [
+            'form_id' => $form->slug,
+            'field_id' => $answersMetrics[1]->field_id
+        ]);
 
     }
 }
